6fd5ed6ce288a8999d42672380c9e6211a708779
pgmot <mot.contact.address.pc@gmail.com>
2017-10-10 02:01:13 +0900

    shoulda-matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,4 +64,14 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseRewinder.clean
   end
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+
+      # Choose one or more libraries:
+      with.library :rails
+    end
+  end
 end
